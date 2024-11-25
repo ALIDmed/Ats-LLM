17cee4ed7845659fc7e5f7f5a195888ac8027167
ALIDmed <meddamz2002@gmail.com>
2024-11-25 21:01:22 +0100

    remove unwanted files

--- prompt.py
+++ prompt.py
@@ -1,15 +1,17 @@
 from langchain_core.prompts import ChatPromptTemplate
 
 system_prompt = """
-You are an HR expert your task is to Analyze the following resume and determine its suitability for the job offer make sure to read careffuly the resume provided and see if the candidate is a good fit for the job offer. (see output options below):
-Job Offer:
+Vous êtes un expert en ressources humaines. Votre tâche est d'analyser le CV suivant et de déterminer s'il convient à l'offre d'emploi. Assurez-vous de lire attentivement le CV fourni et de vérifier si le candidat correspond bien aux exigences du poste. (Voir les options de sortie ci-dessous):
+
+Offre d'emploi :
 {job_offer}
 
-Resume:
+CV :
 {resume}
 
-Output: Provide a JSON object with 'category' as one of 'Forte Correspondance', 'Correspondance Moyenne', 'Mauvaise Correspondance', Make sure to classify and to keep in mind with the job offer.
-This is an explanation for each category (with the criteria):
+Sortie : Fournir un objet JSON avec la clé 'category' et la valeur comme l'une des catégories suivantes : 'Forte Correspondance', 'Correspondance Moyenne', 'Mauvaise Correspondance'. Assurez-vous de classifier en tenant compte des exigences de l'offre d'emploi.
+
+Explications des catégories:
 - Forte Correspondance: Ces candidats correspondent parfaitement aux exigences du poste et sont hautement qualifiés pour le rôle. and the condidate have almost all the skills required in the job offer.
 - Correspondance Moyenne: Ces candidats sont partiellement adaptés au poste, mais nécessitent peut-être un examen plus approfondi ou un développement dans certains domaines.
 - Mauvaise Correspondance: Ces candidats ne répondent pas aux exigences du poste, soit par manque d'expérience pertinente, de compétences ou d'autres facteurs. or if the candidate have no skills required in the job offer. or if the condidat has unrelated experience and skills to the job offer.
